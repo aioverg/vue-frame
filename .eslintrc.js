54422a1d516b79112c14b3a443f580cfe7f4376d
aioverg <aioverg@outlook.com>
2022-03-02 17:00:03 +0800

    修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   "rules": {
     "vue/no-v-modal-argument": "off", // 解决 v-modal 检查报错
+    "parser": "vue-eslint-parser",
   },
-  "parser": "vue-eslint-parser",
   "parserOptions": {
     "ecmaVersion": 7,
     "sourceType": "module"
